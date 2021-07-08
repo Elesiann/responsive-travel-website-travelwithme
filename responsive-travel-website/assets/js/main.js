6769a4253ae43be343004b20b7e84fb50047779b
Giovani <giovanimcorrea18@gmail.com>
2021-07-08 11:09:10 -0300

    adicionando varias features

--- responsive-travel-website/assets/js/main.js
+++ responsive-travel-website/assets/js/main.js
@@ -1,32 +1,56 @@
 /* ======== SHOW MENU ======== */
-const navMenu = document.getElementById('nav-menu'),
-  navToggle = document.getElementById('nav-toggle'),
-  navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /* ======== MENU SHOWN ======== */
 /* VALIDATE IF CONSTANT EXISTS */
 if (navToggle) {
   navToggle.addEventListener("click", () => {
-    navMenu.classList.add('show-menu');
+    navMenu.classList.add("show-menu");
   });
 }
 
 /* ======== MENU HIDDEN ======== */
 /* VALIDATE IF CONSTANT EXISTS */
-if(navClose){
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
-
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /* ======== REMOVE MENU MOBILE ======== */
+const navLink = document.querySelectorAll(".nav__link");
 
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction (){
-    const navMenu = document.getElementById('nav-menu')
-    // When clicking on each nav__link, remove the show-menu class
-    navMenu.classList.remove('show-menu')
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When clicking on each nav__link, remove the show-menu class
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/* ============= CHANGE BACKGROUND HEADER ============= */
+function scrollHeader() {
+  const header = document.getElementById("header");
+  //When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 100) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+/* ======== SWIPER DISCOVER ======== */
+var swiper = new Swiper(".discover__container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  spaceBetween: 32,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
